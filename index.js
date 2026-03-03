3c1b9b041f759576d6791d88512ac610c388c9a8
Abdul Adnan <peerabduladnan@gmail.com>
2026-03-03 14:28:16 +0530

    Added defer and login presistance

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const input = document.getElementById('taskInput')
 const btn = document.getElementById('addBtn')
 const list = document.getElementById('taskList')
 
+document.addEventListener('DOMContentLoaded', loadTasks)
+
 btn.addEventListener('click', addTask)
 
 input.addEventListener('keydown', (e) => {
-    if(e.key === 'Enter') addTask()
+    if (e.key === 'Enter') addTask()
 })
 
-function addTask() {
-  const task = input.value.trim()
-  if(task === "") return
+function addTask(taskText = "", completed = false) {
+    const task = taskText || input.value.trim()
+    if (task === "") return
 
-  const li = document.createElement("li")
+    const li = document.createElement("li")
 
-  li.innerHTML = `
-  <span class='task-text'>${task}</span>
+    li.innerHTML = `
+  <span class='task-text ${completed ? "completed" : ""}'>${task}</span>
   <div>
   <button class="completeBtn">✓</button>
   <button class="editBtn">Edit</button>
@@ -23,23 +25,51 @@ function addTask() {
   </div>
   `
 
-  list.appendChild(li)
-  input.value = ""
+    list.appendChild(li)
+    input.value = ""
 
-  li.querySelector('.deleteBtn').addEventListener('click', () => {
-    li.remove()
-  })
+    //for delete
+    li.querySelector('.deleteBtn').addEventListener('click', () => {
+        li.remove()
+        saveTasks()
+    })
 
-  li.querySelector('.completeBtn').addEventListener('click', () => {
-    li.querySelector('.task-text').classList.toggle('completed')
-  })
+    //for complete
+    li.querySelector('.completeBtn').addEventListener('click', () => {
+        li.querySelector('.task-text').classList.toggle('completed')
+        saveTasks()
+    })
 
-  li.querySelector('.editBtn').addEventListener('click', () => {
-    const textElement = li.querySelector('.task-text')
-    const newTask = prompt('Updated your task: ', textElement.textContent)
+    //for edit
+    li.querySelector('.editBtn').addEventListener('click', () => {
+        const textElement = li.querySelector('.task-text')
+        const newTask = prompt('Updated your task: ', textElement.textContent)
 
-    if(newTask !== null && newTask.trim() !== "") {
-        textElement.textContent = newTask.trim()
-    }
-  })
+        if (newTask !== null && newTask.trim() !== "") {
+            textElement.textContent = newTask.trim()
+            saveTasks()
+        }
+    })
+    saveTasks()
+}
+
+function saveTasks() {
+    const tasks = []
+
+    document.querySelectorAll('#taskList li').forEach((li) => {
+
+        tasks.push({ 
+            text: li.querySelector('.task-text').textContent,
+            completed: li.querySelector('.task-text').classList.contains('completed')
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+    tasks.forEach(task => 
+        addTask(task.text, task.completed)
+    )
 }
